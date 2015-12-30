a4cec46ae713a55989c1d47a9e99b27596415f98
Youngung Jeong <youngung.jeong@gmail.com>
2015-12-30 09:35:09 -0500

    Added strain-hardening equations

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 setup(name='MPL_personal_lib',
-      version='0.1',
+      version='0.2',
       description='collection of personal matplotlib lib',
       author='Youngung Jeong',
       author_email='youngung.jeong@gmail.com',
-      packages=['MP','MP.lib','MP.mat','MP.opt'],
+      packages=['MP','MP.lib','MP.mat','MP.opt','MP.cal'],
       package_dir={'MP':'src',
                    'MP.lib':'src/lib',
                    'MP.mat':'src/mat',
-                   'MP.opt':'src/opt'}
+                   'MP.opt':'src/opt',
+                   'MP.cal':'src/cal'}
       )
