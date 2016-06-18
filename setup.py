da899bb3fb86ed8e7ff038da0e42443f6080144f
Youngung Jeong <youngung.jeong@gmail.com>
2016-06-18 14:38:44 -0400

    Specimen geometry added

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(name='MPL_personal_lib',
       description='collection of personal matplotlib lib',
       author='Youngung Jeong',
       author_email='youngung.jeong@gmail.com',
-      packages=['MP','MP.lib','MP.mat','MP.opt','MP.cal'],
+      packages=['MP','MP.lib','MP.mat','MP.opt','MP.cal','MP.geom'],
       package_dir={'MP':'src',
                    'MP.lib':'src/lib',
                    'MP.mat':'src/mat',
                    'MP.opt':'src/opt',
-                   'MP.cal':'src/cal'}
+                   'MP.cal':'src/cal',
+                   'MP.geom':'src/geom'}
       )
