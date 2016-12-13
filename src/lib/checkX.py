f9297331646f5bf4143252e8357f2cda6d5a1f5b
Youngung Jeong <totorounga@postech.ac.kr>
2016-12-13 10:30:39 +0900

    remove temp files after checking x server

--- src/lib/checkX.py
+++ src/lib/checkX.py
@@ -10,8 +10,11 @@ import os, subprocess
 
 def main(verbose=True):
     """
+    Arguments
+    ---------
+    verbose
     """
-    import temp#.gen_tempfile
+    import temp
     fn_script = temp.gen_tempfile()
     fn_stdo   = temp.gen_tempfile()
     fn_stde   = temp.gen_tempfile()
@@ -37,6 +40,9 @@ def main(verbose=True):
             print fo.read()
 
     stdo.close(); stde.close()
+
+    ## delete temp files
+    os.remove(fn_stdo); os.remove(fn_stde)
     return rst
 
 if __name__=='__main__':
